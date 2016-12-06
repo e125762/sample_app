60def32f7d928cd8bcf9a87e9464d249272e2fb2
Keito <e125762@ie.u-ryukyu.ac.jp>
2016-12-06 07:15:13 +0000

    Finish login/logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,4 +64,8 @@ password: "foobar", password_confirmation: "foobar")
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
